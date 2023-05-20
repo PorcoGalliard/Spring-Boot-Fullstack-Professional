78f311df7bacf5265c10b1930d19db99ffd62169
PorcoGalliard <nanasuryana335@gmail.com>
2023-05-20 23:17:15 +0700

    feat: create JDBC class to implements JDBC template

--- src/main/java/com/amigoscode/customer/CustomerJDBCDataAccessService.java
+++ src/main/java/com/amigoscode/customer/CustomerJDBCDataAccessService.java
@@ -1,2 +1,44 @@
-package com.amigoscode.customer;public class CustomerJDBCDataAccessService {
+package com.amigoscode.customer;
+
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository("jdbc")
+public class CustomerJDBCDataAccessService implements CustomerDao {
+    @Override
+    public List<Customer> selectAllCustomers() {
+        return null;
+    }
+
+    @Override
+    public Optional<Customer> selectCustomerById(Integer id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public void insertCustomer(Customer customer) {
+
+    }
+
+    @Override
+    public boolean existPersonWithEmail(String email) {
+        return false;
+    }
+
+    @Override
+    public boolean existPersonWithId(Integer id) {
+        return false;
+    }
+
+    @Override
+    public void deleteCustomerById(Integer id) {
+
+    }
+
+    @Override
+    public void updateCustomer(Customer update) {
+
+    }
 }
