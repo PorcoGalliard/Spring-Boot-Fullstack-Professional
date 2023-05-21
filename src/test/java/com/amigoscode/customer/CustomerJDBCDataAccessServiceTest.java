3a43b0c9818ed3e5db51f651ea539db66c641c52
PorcoGalliard <nanasuryana335@gmail.com>
2023-05-21 17:06:18 +0700

    feat: implement initialization of JDBC testing

--- src/test/java/com/amigoscode/customer/CustomerJDBCDataAccessServiceTest.java
+++ src/test/java/com/amigoscode/customer/CustomerJDBCDataAccessServiceTest.java
@@ -1,4 +1,85 @@
+package com.amigoscode.customer;
+
+import com.amigoscode.AbstractTestcontainersUnitTest;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.jdbc.core.JdbcTemplate;
+
 import static org.junit.jupiter.api.Assertions.*;
-class CustomerJDBCDataAccessServiceTest {
-  
+
+class CustomerJDBCDataAccessServiceTest extends AbstractTestcontainersUnitTest {
+
+    private CustomerJDBCDataAccessService underTest;
+    private final CustomerRowMapper customerRowMapper = new CustomerRowMapper();
+
+    @BeforeEach
+    void setUp() {
+        underTest = new CustomerJDBCDataAccessService(
+                getJdbcTemplate(),
+                customerRowMapper
+        );
+    }
+
+    @Test
+    void selectAllCustomers() {
+        //Given
+
+        //When
+
+        //Then
+    }
+
+    @Test
+    void selectCustomerById() {
+        //Given
+
+        //When
+
+        //Then
+    }
+
+    @Test
+    void insertCustomer() {
+        //Given
+
+        //When
+
+        //Then
+    }
+
+    @Test
+    void existPersonWithEmail() {
+        //Given
+
+        //When
+
+        //Then
+    }
+
+    @Test
+    void existPersonWithId() {
+        //Given
+
+        //When
+
+        //Then
+    }
+
+    @Test
+    void deleteCustomerById() {
+        //Given
+
+        //When
+
+        //Then
+    }
+
+    @Test
+    void updateCustomer() {
+        //Given
+
+        //When
+
+        //Then
+    }
 }
