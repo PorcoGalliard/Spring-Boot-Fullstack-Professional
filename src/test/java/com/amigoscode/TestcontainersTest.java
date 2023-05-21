8601c543be53014d0b1ed96ee2354bf2ab871508
PorcoGalliard <nanasuryana335@gmail.com>
2023-05-21 15:05:36 +0700

    feat: implement canStartPostgreSQLDB test

--- src/test/java/com/amigoscode/TestcontainersTest.java
+++ src/test/java/com/amigoscode/TestcontainersTest.java
@@ -5,20 +5,20 @@ import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 @Testcontainers
 public class TestcontainersTest {
+
     @Container
     private static final PostgreSQLContainer<?> postgreSQLContainer =
-            new PostgreSQLContainer<>("postgres::latest")
+            new PostgreSQLContainer<>("postgres:latest")
                     .withDatabaseName("amigoscode-dao-unit-test")
                     .withUsername("amigoscode")
                     .withPassword("password");
     @Test
-    void canStartPostgresDB() {
-        //Given
-
-        //When
-
-        //Then
+    void canStartPostgreSQLDB() {
+        assertThat(postgreSQLContainer.isRunning()).isTrue();
+        assertThat(postgreSQLContainer.isCreated()).isTrue();
     }
 }
