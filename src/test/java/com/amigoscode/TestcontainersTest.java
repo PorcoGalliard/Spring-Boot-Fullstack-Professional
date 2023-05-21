7a34526a38d121fdf8d65eaba95e23178657019b
PorcoGalliard <nanasuryana335@gmail.com>
2023-05-21 14:26:10 +0700

    feat: create initial container test for PostgreSQL

--- src/test/java/com/amigoscode/TestcontainersTest.java
+++ src/test/java/com/amigoscode/TestcontainersTest.java
@@ -1,2 +1,24 @@
-package com.amigoscode;public class TestcontainersTest {
+package com.amigoscode;
+
+import org.junit.jupiter.api.Test;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@Testcontainers
+public class TestcontainersTest {
+    @Container
+    private static final PostgreSQLContainer<?> postgreSQLContainer =
+            new PostgreSQLContainer<>("postgres::latest")
+                    .withDatabaseName("amigoscode-dao-unit-test")
+                    .withUsername("amigoscode")
+                    .withPassword("password");
+    @Test
+    void canStartPostgresDB() {
+        //Given
+
+        //When
+
+        //Then
+    }
 }
