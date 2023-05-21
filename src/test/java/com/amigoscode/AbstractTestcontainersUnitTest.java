ecae51d027869a3587b98dd0208f83ef8648492a
PorcoGalliard <nanasuryana335@gmail.com>
2023-05-21 16:40:15 +0700

    feat: implement the abstract class of unit test

--- src/test/java/com/amigoscode/AbstractTestcontainersUnitTest.java
+++ src/test/java/com/amigoscode/AbstractTestcontainersUnitTest.java
@@ -1,2 +1,51 @@
-package com.amigoscode;public class AbstractTestcontainersUnitTest {
+package com.amigoscode;
+
+import org.flywaydb.core.Flyway;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Testcontainers
+public abstract class AbstractTestcontainersUnitTest {
+
+    @BeforeAll
+    static void beforeAll() {
+        Flyway flyway = Flyway.configure().dataSource(
+                        postgreSQLContainer.getJdbcUrl(),
+                        postgreSQLContainer.getUsername(),
+                        postgreSQLContainer.getPassword())
+                .load();
+        flyway.migrate();
+    }
+
+    @Container
+    protected static final PostgreSQLContainer<?> postgreSQLContainer =
+            new PostgreSQLContainer<>("postgres:latest")
+                    .withDatabaseName("amigoscode-dao-unit-test")
+                    .withUsername("amigoscode")
+                    .withPassword("password");
+
+    @DynamicPropertySource
+    private static void registerDataSourceProperties(DynamicPropertyRegistry registry) {
+        registry.add(
+                "spring.datasource.url",
+                postgreSQLContainer::getJdbcUrl
+        );
+
+        registry.add(
+                "spring.datasource.username",
+                postgreSQLContainer::getUsername
+        );
+
+        registry.add(
+                "spring.datasource.password",
+                postgreSQLContainer::getPassword
+        );
+    }
 }
