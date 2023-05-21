8e7d3d849c0ef3ad79ef2b6c8fb4a5a57e27b966
PorcoGalliard <nanasuryana335@gmail.com>
2023-05-21 17:07:20 +0700

    feat: implement getDataSource and getJdbcTemplate for JdbcTemplate constructor

--- src/test/java/com/amigoscode/AbstractTestcontainersUnitTest.java
+++ src/test/java/com/amigoscode/AbstractTestcontainersUnitTest.java
@@ -3,11 +3,15 @@ package com.amigoscode;
 import org.flywaydb.core.Flyway;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
+
+import javax.sql.DataSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -47,5 +51,17 @@ public abstract class AbstractTestcontainersUnitTest {
                 "spring.datasource.password",
                 postgreSQLContainer::getPassword
         );
+    }
+
+    private static DataSource getDataSource() {
+       return DataSourceBuilder.create()
+                .driverClassName(postgreSQLContainer.getDriverClassName())
+                .url(postgreSQLContainer.getJdbcUrl())
+                .username(postgreSQLContainer.getUsername())
+                .password(postgreSQLContainer.getPassword()).build();
+    }
+
+    protected static JdbcTemplate getJdbcTemplate() {
+        return new JdbcTemplate(getDataSource());
     }
 }
