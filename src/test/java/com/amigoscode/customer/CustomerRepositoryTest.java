209ace8809addf95b1db99b4dbcb2027e6e77f5c
PorcoGalliard <nanasuryana335@gmail.com>
2023-05-24 23:02:12 +0700

    feat: implement existCustomerByEmail and existCustomerById test

--- src/test/java/com/amigoscode/customer/CustomerRepositoryTest.java
+++ src/test/java/com/amigoscode/customer/CustomerRepositoryTest.java
@@ -1,4 +1,102 @@
+package com.amigoscode.customer;
+
+import com.amigoscode.AbstractTestcontainersUnitTest;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.ApplicationContext;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
-class CustomerRepositoryTest {
-  
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class CustomerRepositoryTest extends AbstractTestcontainersUnitTest {
+
+    @Autowired
+    private CustomerRepository underTest;
+
+    @Autowired
+    private ApplicationContext applicationContext;
+
+    @BeforeEach
+    void setUp() {
+        underTest.deleteAll();
+        System.out.println(applicationContext.getBeanDefinitionCount());
+    }
+
+    @Test
+    void existsCustomerByEmail() {
+        //Given
+        String email = FAKER.internet().safeEmailAddress() + "-" + UUID.randomUUID();
+        Customer customer = new Customer(
+                FAKER.name().fullName(),
+                email,
+                20
+        );
+
+        underTest.save(customer);
+
+        //When
+        var actual = underTest.existsCustomerByEmail(email);
+
+        //Then
+        assertThat(actual).isTrue();
+    }
+
+    @Test
+    void existsCustomerByEmailWhenFails() {
+        //Given
+        String email = FAKER.internet().safeEmailAddress() + "-" + UUID.randomUUID();
+
+        //When
+        var actual = underTest.existsCustomerByEmail(email);
+
+        //Then
+        assertThat(actual).isFalse();
+    }
+
+    @Test
+    void existsCustomerById() {
+        //Given
+        String email = FAKER.internet().safeEmailAddress() + "-" + UUID.randomUUID();
+        Customer customer = new Customer(
+                FAKER.name().fullName(),
+                email,
+                20
+        );
+
+        underTest.save(customer);
+
+        Integer id = underTest.findAll()
+                .stream()
+                .filter(c -> c.getEmail().equals(email))
+                .map(Customer::getId)
+                .findFirst()
+                .orElseThrow();
+
+        //When
+        var actual = underTest.existsCustomerById(id);
+
+        //Then
+        assertThat(actual).isTrue();
+    }
+
+    @Test
+    void existsCustomerByIdWhenNotPresent() {
+        //Given
+
+        Integer id = -1;
+
+        //When
+        var actual = underTest.existsCustomerById(id);
+
+        //Then
+        assertThat(actual).isFalse();
+    }
 }
