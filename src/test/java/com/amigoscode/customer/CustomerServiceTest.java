edd4e44cc0dc96eb5577a94c957ca5b03b8bc15a
PorcoGalliard <nanasuryana335@gmail.com>
2023-05-26 04:29:01 +0700

    feat: implement initial test code

--- src/test/java/com/amigoscode/customer/CustomerServiceTest.java
+++ src/test/java/com/amigoscode/customer/CustomerServiceTest.java
@@ -1,4 +1,65 @@
+package com.amigoscode.customer;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
 import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
 class CustomerServiceTest {
-  
+
+    private CustomerDao customerDao;
+    private CustomerService underTest;
+
+    @BeforeEach
+    void setUp() {
+        underTest = new CustomerService(customerDao);
+    }
+
+    @Test
+    void getAllCustomers() {
+        //Given
+
+        //When
+
+        //Then
+    }
+
+    @Test
+    void getCustomer() {
+        //Given
+
+        //When
+
+        //Then
+    }
+
+    @Test
+    void addCustomer() {
+        //Given
+
+        //When
+
+        //Then
+    }
+
+    @Test
+    void deleteCustomerById() {
+        //Given
+
+        //When
+
+        //Then
+    }
+
+    @Test
+    void updateCustomer() {
+        //Given
+
+        //When
+
+        //Then
+    }
 }
