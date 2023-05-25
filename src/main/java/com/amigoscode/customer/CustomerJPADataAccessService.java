b6f40c76a10b4c8055ce826120c120d389feef23
PorcoGalliard <nanasuryana335@gmail.com>
2023-05-25 07:08:05 +0700

    feat: refactor the code clarity

--- src/main/java/com/amigoscode/customer/CustomerJPADataAccessService.java
+++ src/main/java/com/amigoscode/customer/CustomerJPADataAccessService.java
@@ -6,7 +6,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Repository("jpa")
-public class CustomerJPADataAccessService implements CustomerDao{
+public class CustomerJPADataAccessService implements CustomerDao {
 
     private final CustomerRepository customerRepository;
 
