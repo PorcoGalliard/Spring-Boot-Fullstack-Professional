d5f4b6fb6137d60bdfe0c604aa28741a442d8653
PorcoGalliard <nanasuryana335@gmail.com>
2023-05-25 07:10:22 +0700

    feat: create initial test method and before-after test

--- src/test/java/com/amigoscode/customer/CustomerJPADataAccessServiceTest.java
+++ src/test/java/com/amigoscode/customer/CustomerJPADataAccessServiceTest.java
@@ -1,4 +1,81 @@
+package com.amigoscode.customer;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class CustomerJPADataAccessServiceTest {
-  
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void selectAllCustomers() {
+        //Given
+
+        //When
+
+        //Then
+    }
+
+    @Test
+    void selectCustomerById() {
+        //Given
+
+        //When
+
+        //Then
+    }
+
+    @Test
+    void insertCustomer() {
+        //Given
+
+        //When
+
+        //Then
+    }
+
+    @Test
+    void deleteCustomerById() {
+        //Given
+
+        //When
+
+        //Then
+    }
+
+    @Test
+    void updateCustomer() {
+        //Given
+
+        //When
+
+        //Then
+    }
+
+    @Test
+    void existPersonWithEmail() {
+        //Given
+
+        //When
+
+        //Then
+    }
+
+    @Test
+    void existPersonWithId() {
+        //Given
+
+        //When
+
+        //Then
+    }
 }
