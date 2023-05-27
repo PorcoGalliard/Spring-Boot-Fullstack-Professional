91ceb8be73e73207ac622f425579bb2baa469a8e
PorcoGalliard <nanasuryana335@gmail.com>
2023-05-27 16:58:58 +0700

    feat: create CustomerRowMapperTest file

---
+++ src/test/java/com/amigoscode/customer/CustomerRowMapperTest.java
@@ -0,0 +1,18 @@
+package com.amigoscode.customer;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CustomerRowMapperTest {
+
+    @Test
+    void mapRow() {
+        //Given
+        CustomerRowMapper customerRowMapper = new CustomerRowMapper();
+
+        //When
+
+        //Then
+    }
+}
