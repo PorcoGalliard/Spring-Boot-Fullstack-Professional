e612794cb1397234e99243b00962d63682717611
PorcoGalliard <nanasuryana335@gmail.com>
2023-05-27 23:39:04 +0700

    feat: create initial setup for integration test

--- src/test/java/com/amigoscode/journey/CustomerIntegrationTest.java
+++ src/test/java/com/amigoscode/journey/CustomerIntegrationTest.java
@@ -1,2 +1,14 @@
-package com.amigoscode.journey;public class CustomerIntegrationTest {
+package com.amigoscode.journey;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.*;
+
+@SpringBootTest(webEnvironment = RANDOM_PORT)
+public class CustomerIntegrationTest {
+
+    @Autowired
+    private WebTestClient webTestClient;
 }
