6c9cc7682e47e394b11a0df09a418f1b4c5184eb
PorcoGalliard <nanasuryana335@gmail.com>
2023-05-28 16:41:56 +0700

    feat: implement getAllCustomer and the present of it, integration test

--- src/test/java/com/amigoscode/journey/CustomerIntegrationTest.java
+++ src/test/java/com/amigoscode/journey/CustomerIntegrationTest.java
@@ -1,18 +1,22 @@
 package com.amigoscode.journey;
 
+import com.amigoscode.customer.Customer;
 import com.amigoscode.customer.CustomerRegistrationRequest;
 import com.github.javafaker.Faker;
 import com.github.javafaker.Name;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import reactor.core.publisher.Mono;
 
+import java.util.List;
 import java.util.Random;
 import java.util.UUID;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.*;
 
 @SpringBootTest(webEnvironment = RANDOM_PORT)
@@ -21,6 +25,7 @@ public class CustomerIntegrationTest {
     @Autowired
     private WebTestClient webTestClient;
 
+    private static final String CUSTOMER_URI = "/api/v1/customers";
     private static final Random RANDOM = new Random();
 
     @Test
@@ -31,7 +36,7 @@ public class CustomerIntegrationTest {
         Faker faker = new Faker();
         Name fakerName = faker.name();
         String name = fakerName.fullName();
-        String email = fakerName.lastName() + UUID.randomUUID() + "@amigoscode.com";
+        String email = fakerName.lastName() + UUID.randomUUID() + "@uncf.com";
         int age = RANDOM.nextInt(1, 100);
 
         CustomerRegistrationRequest request = new CustomerRegistrationRequest(
@@ -40,15 +45,33 @@ public class CustomerIntegrationTest {
 
         // Send a post request
         webTestClient.post()
-                .uri("/api/v1/customers")
+                .uri(CUSTOMER_URI)
                 .accept(MediaType.APPLICATION_JSON)
                 .contentType(MediaType.APPLICATION_JSON)
                 .body(Mono.just(request), CustomerRegistrationRequest.class)
                 .exchange()
                 .expectStatus()
                 .isOk();
-        //When
 
-        //Then
+        // Get all customers
+        List<Customer> allCustomers = webTestClient.get()
+                .uri(CUSTOMER_URI)
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus()
+                .isOk()
+                .expectBodyList(new ParameterizedTypeReference<Customer>() {
+                })
+                .returnResult()
+                .getResponseBody();
+
+        // Make sure customer is present
+        Customer expectedCustomer = new Customer(
+                name, email, age
+        );
+
+        assertThat(allCustomers).usingRecursiveFieldByFieldElementComparatorIgnoringFields("id")
+                .contains(expectedCustomer);
+
     }
 }
