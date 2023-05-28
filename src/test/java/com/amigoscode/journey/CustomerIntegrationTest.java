945e458e7d57758d2d4c5971add934af94790c03
PorcoGalliard <nanasuryana335@gmail.com>
2023-05-28 16:04:44 +0700

    feat: create process of integration test of registration and post request

--- src/test/java/com/amigoscode/journey/CustomerIntegrationTest.java
+++ src/test/java/com/amigoscode/journey/CustomerIntegrationTest.java
@@ -1,8 +1,17 @@
 package com.amigoscode.journey;
 
+import com.amigoscode.customer.CustomerRegistrationRequest;
+import com.github.javafaker.Faker;
+import com.github.javafaker.Name;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Mono;
+
+import java.util.Random;
+import java.util.UUID;
 
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.*;
 
@@ -11,4 +20,35 @@ public class CustomerIntegrationTest {
 
     @Autowired
     private WebTestClient webTestClient;
+
+    private static final Random RANDOM = new Random();
+
+    @Test
+    void canRegisterACustomer() {
+        //Given
+
+        // Create Registration request
+        Faker faker = new Faker();
+        Name fakerName = faker.name();
+        String name = fakerName.fullName();
+        String email = fakerName.lastName() + UUID.randomUUID() + "@amigoscode.com";
+        int age = RANDOM.nextInt(1, 100);
+
+        CustomerRegistrationRequest request = new CustomerRegistrationRequest(
+            name, email, age
+        );
+
+        // Send a post request
+        webTestClient.post()
+                .uri("/api/v1/customers")
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(Mono.just(request), CustomerRegistrationRequest.class)
+                .exchange()
+                .expectStatus()
+                .isOk();
+        //When
+
+        //Then
+    }
 }
