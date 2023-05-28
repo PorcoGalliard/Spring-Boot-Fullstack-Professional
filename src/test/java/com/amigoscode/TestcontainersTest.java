49a127419c0b511414240b6c55c2c59300540e7c
PorcoGalliard <nanasuryana335@gmail.com>
2023-05-28 21:09:03 +0700

    refactor: refactor class file

--- src/test/java/com/amigoscode/TestcontainersTest.java
+++ src/test/java/com/amigoscode/TestcontainersTest.java
@@ -1,5 +1,6 @@
 package com.amigoscode;
 
+import com.amigoscode.customer.Customer;
 import org.flywaydb.core.Flyway;
 import org.junit.jupiter.api.Test;
 import org.springframework.test.context.DynamicPropertyRegistry;
@@ -7,6 +8,8 @@ import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
+
+import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
