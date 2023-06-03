cb55bb1fda80e52a582e9a43af95b23229e47cdb
PorcoGalliard <nanasuryana335@gmail.com>
2023-06-03 23:40:41 +0700

    style: change PingPongController to a real controller to test the CD again

--- backend/src/main/java/com/amigoscode/PingPongController.java
+++ backend/src/main/java/com/amigoscode/PingPongController.java
@@ -1,8 +1,13 @@
 package com.amigoscode;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class PingPongController {
     record PingPong (String submit) {}
 
+    @GetMapping("/ping")
     public PingPong getPingPong() {
         return new PingPong("ayolahh");
     }
