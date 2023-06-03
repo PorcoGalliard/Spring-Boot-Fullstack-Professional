5da8a2e5f069450916b8e9410a47fc70fb011c56
PorcoGalliard <nanasuryana335@gmail.com>
2023-06-03 11:24:47 +0700

    refactor: delete a line and unused import

--- backend/src/main/java/com/amigoscode/Main.java
+++ backend/src/main/java/com/amigoscode/Main.java
@@ -8,7 +8,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-import java.util.List;
 import java.util.Random;
 
 @SpringBootApplication
@@ -16,7 +15,6 @@ public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
     }
-
 
     @Bean
     CommandLineRunner runner(CustomerRepository customerRepository) {
