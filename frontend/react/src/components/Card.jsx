e1273041a645abaf9fa1cad01afe1b591be32c31
PorcoGalliard <nanasuryana335@gmail.com>
2023-06-13 20:42:15 +0700

    feat: implement card to contains user profile

--- frontend/react/src/components/Card.jsx
+++ frontend/react/src/components/Card.jsx
@@ -8,14 +8,14 @@ import {
     Text,
     Stack,
     Button,
-    useColorModeValue,
+    useColorModeValue, Tag,
 } from '@chakra-ui/react';
 
-export default function CardWithImage() {
+export default function CardWithImage({id, name, email, age}) {
     return (
         <Center py={6}>
             <Box
-                maxW={'270px'}
+                maxW={'300px'}
                 w={'full'}
                 bg={useColorModeValue('white', 'gray.800')}
                 boxShadow={'2xl'}
@@ -43,40 +43,15 @@ export default function CardWithImage() {
                 </Flex>
 
                 <Box p={6}>
-                    <Stack spacing={0} align={'center'} mb={5}>
+                    <Stack spacing={2} align={'center'} mb={5}>
+                        <Tag borderRadius={'full'}>{id}</Tag>
                         <Heading fontSize={'2xl'} fontWeight={500} fontFamily={'body'}>
-                            John Doe
+                            {name}
                         </Heading>
-                        <Text color={'gray.500'}>Frontend Developer</Text>
+                        <Text color={'gray.500'}>{email}</Text>
+                        <Text color={'gray.500'}>Age {age}</Text>
                     </Stack>
 
-                    <Stack direction={'row'} justify={'center'} spacing={6}>
-                        <Stack spacing={0} align={'center'}>
-                            <Text fontWeight={600}>23k</Text>
-                            <Text fontSize={'sm'} color={'gray.500'}>
-                                Followers
-                            </Text>
-                        </Stack>
-                        <Stack spacing={0} align={'center'}>
-                            <Text fontWeight={600}>23k</Text>
-                            <Text fontSize={'sm'} color={'gray.500'}>
-                                Followers
-                            </Text>
-                        </Stack>
-                    </Stack>
-
-                    <Button
-                        w={'full'}
-                        mt={8}
-                        bg={useColorModeValue('#151f21', 'gray.900')}
-                        color={'white'}
-                        rounded={'md'}
-                        _hover={{
-                            transform: 'translateY(-2px)',
-                            boxShadow: 'lg',
-                        }}>
-                        Follow
-                    </Button>
                 </Box>
             </Box>
         </Center>
