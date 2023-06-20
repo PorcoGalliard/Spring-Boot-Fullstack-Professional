bad786b842511bffd93bbdc88f131999dd83f9f2
PorcoGalliard <nanasuryana335@gmail.com>
2023-06-20 23:04:02 +0700

    feat: allow /ping to have no authentication

--- backend/src/main/java/com/amigoscode/security/SecurityFilterChainConfig.java
+++ backend/src/main/java/com/amigoscode/security/SecurityFilterChainConfig.java
@@ -37,6 +37,11 @@ public class SecurityFilterChainConfig {
                         "/api/v1/customers",
                         "/api/v1/auth/login")
                 .permitAll()
+                .requestMatchers(
+                        HttpMethod.GET,
+                        "/ping"
+                )
+                .permitAll()
                 .anyRequest()
                 .authenticated()
                 .and()
